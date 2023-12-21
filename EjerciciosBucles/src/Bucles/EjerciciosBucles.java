cd3bd13e61a12700438df4dbd66ccd0b91368eff
HugoRivera <hugorivera060904@gmail.com>
2023-12-21 14:07:49 +0100

    4

--- EjerciciosBucles/src/Bucles/EjerciciosBucles.java
+++ EjerciciosBucles/src/Bucles/EjerciciosBucles.java
@@ -1,5 +1,54 @@
 package Bucles;
 
+import java.util.Scanner;
+
 public class EjerciciosBucles {
+	
+	public static void main(String[] args) {
+	//bucles();
+	ej1();
+	}
+
+
+
+public static void bucles() {
+	
+	int  condicion = 10;
+	for (int i = 0; i<= condicion; i++) {
+		System.out.println("Bucle for");
+		
+	}
+	
+	int parada = 0;
+	for(int i = 10; i >= parada; i--) {
+		System.out.println("Bucle for inverso");
+		
+	}
+	
+	int i = 0; {
+		
+		
+	}
+	while (i < condicion) {
+		
+		System.out.println("Bucle while");
+		i++;
+	}
+	
+}
+
+public static void ej1() {
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.println("Porfavor introduzca una palabra");
+	String palabra = scanner.next();
+	scanner.close();
+for(int i = 0; i < 10; i++) {
+System.out.println(palabra);
+	
+}
+}
+
 
 }
